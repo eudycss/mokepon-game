1f2a9b9829620c89bd1c5b8c9c8096fa7b2ff781
Sebas CS <euddyk@gmail.com>
2022-08-24 22:18:46 -0500

    Random pets with arrangements

--- js/mokepon.js
+++ js/mokepon.js
@@ -141,16 +141,11 @@ function SeleccionarMascotaJugador() {
   seleccionarMascotaEnemigo();
 }
 
+/* Seleccionar Mascota enemigo */
 function seleccionarMascotaEnemigo() {
-  let mascotaAleatoria = aleatorio(1, 3);
+  let mascotaAleatoria = aleatorio(0, mokepones.length-1);
 
-  if (mascotaAleatoria == 1) {
-    spanMascotaEnemigo.innerHTML = "Hipodoge";
-  } else if (mascotaAleatoria == 2) {
-    spanMascotaEnemigo.innerHTML = "Capipepo";
-  } else {
-    spanMascotaEnemigo.innerHTML = "Ratigueya";
-  }
+  spanMascotaEnemigo.innerHTML=mokepones[mascotaAleatoria].name
 }
 
 function ataqueFuego() {
